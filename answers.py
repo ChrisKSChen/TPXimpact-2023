55c2da24588b2b26d0b30f0b3fdd21e7a9ddfac0
ChrisCHENN <747150628@qq.com>
2023-04-17 22:31:15 +0100

    new

--- answers.py
+++ answers.py
@@ -3,7 +3,9 @@ if ans.isalpha() and len(ans)==1:
     num = int(ord(ans.upper())- 64)
     print('The position is {}'.format(num))
 else:
+
     print('Please enter one valEid letter')
+
 
 # username = input("Enterq the character for the Diamond:")
 # print("Username is: " + username)
