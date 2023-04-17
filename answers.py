5ff71a9141cdef22694d3c2f0219ba5eca271a3b
ChrisCHENN <747150628@qq.com>
2023-04-17 23:24:46 +0100

    work

--- answers.py
+++ answers.py
@@ -1,9 +1,27 @@
 ans = input("Enter the character for the Diamond:")
 if ans.isalpha() and len(ans)==1:
-    num = int(ord(ans.upper())- 64)
-    print('The position is {}'.format(num))
+    n = int(ord(ans.upper())- 64)
+    # print('The position is {}'.format(n))
+
+    for i in range(1, n+1):
+        for j in range(1, n-i+1):
+            print(" ", end="")
+        for k in range(1, 2*i):
+            if k==1 or k==2*i-1:
+                print(str(chr(64+i)), end="")
+            else:
+                print(" ", end="")
+        print()
+    
+    for i in range(n-1,0,-1):
+        for j in range(1, n-i+1):
+            print(" ", end="")
+        for k in range(1, 2*i):
+            if k==1 or k==2*i-1:
+                print(str(chr(64+i)), end="")
+            else:
+                print(" ", end="")
+        print()
 else:
     print('Please enter one valid letter')
 
-# username = input("Enterq the character for the Diamond:")
-# print("Username is: " + username)
